32b8978d854b8cf34ccdbec0feff38f133643e0b
quiesscent <ciphedc@gmail.com>
2024-07-10 11:23:51 +0300

    Link Updates

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 const MainLayout = () => {
@@ -8,15 +8,15 @@ const MainLayout = () => {
       <div class="layout">
         <div class="side-nav border-end h-100">
           <ul className='rm navbar-nav-scroll'>
-            <li class="nav-right"><a href="/" class="nav-right">Dashboard</a></li>
-            <li class="nav-right"><a href="/employees" class="nav-right">Employees</a></li>
-            <li class="nav-right"><a href="/products" class="nav-right">Products</a></li>
-            <li class="nav-right"><a href="/creation" class="nav-right">Create Product</a></li>
-            <li class="nav-right"><a href="/sales" class="nav-right">Sales</a></li>
-            <li class="nav-right"><a href="/stock" class="nav-right">Stock</a></li>
-            <li class="nav-right"><a href="/inventory" class="nav-right">Inventory</a></li>
-            <li class="nav-right"><a href="/orders" class="nav-right">Orders</a></li>
-            <li class="nav-right"><a href="/pos" class="nav-right">POS</a></li>
+            <li class="nav-right"><Link to="/" class="nav-right">Dashboard</Link></li>
+            <li class="nav-right"><Link to="/employees" class="nav-right">Employees</Link></li>
+            <li class="nav-right"><Link to="/products" class="nav-right">Products</Link></li>
+            <li class="nav-right"><Link to="/creation" class="nav-right">Create Product</Link></li>
+            <li class="nav-right"><Link to="/sales" class="nav-right">Sales</Link></li>
+            <li class="nav-right"><Link to="/stock" class="nav-right">Stock</Link></li>
+            <li class="nav-right"><Link to="/inventory" class="nav-right">Inventory</Link></li>
+            <li class="nav-right"><Link to="/orders" class="nav-right">Orders</Link></li>
+            <li class="nav-right"><Link to="/pos" class="nav-right">POS</Link></li>
           </ul>
         </div>
         <div class="content">
